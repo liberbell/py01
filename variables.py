8822a4a2dcf2bd3791b0e84b14134917d9e51ed7
Liberbell <liberbell@gmail.com>
2019-05-03 15:39:21 +0900

    variables6

--- variables.py
+++ variables.py
@@ -1,7 +1,11 @@
 f = 0
-print(f)
+# print(f)
 
-f = 'abc'
-print(f)
+# f = 'abc'
+# print(f)
 
-print('This is a string' + str(123))
+# print('This is a string' + str(123))
+
+def someFunction():
+    f = 'def'
+    print(f)
