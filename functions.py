bf723e6c6b432ee5601115cccb812970ebc9d234
Liberbell <liberbell@gmail.com>
2019-05-04 10:36:12 +0900

    functions3

--- functions.py
+++ functions.py
@@ -8,6 +8,12 @@ def func2(arg1, arg2):
 def cube(x):
     return x*x*x
 
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
+
 # func1()
 # print(func1())
 # print(func1)
