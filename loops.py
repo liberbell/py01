66373368dbc8c9bd55bfc915e8468c15250bc082
Liberbell <liberbell@gmail.com>
2019-05-05 19:09:16 +0900

    loops4

--- loops.py
+++ loops.py
@@ -4,8 +4,12 @@ def main():
     #     print(x)
     #     x = x + 1
 
-    for x in range(5, 10):
-        print(x)
+    # for x in range(5, 10):
+    #     print(x)
+
+    days = ["Mon", "Thu", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for d in days:
+        print(d)
 
 
 if __name__ == '__main__':
