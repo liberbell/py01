716405562b0d63ba6942d18b2dd3405c517a6939
Liberbell <liberbell@gmail.com>
2019-05-05 19:12:10 +0900

    loops6

--- loops.py
+++ loops.py
@@ -11,7 +11,8 @@ def main():
     # for d in days:
     #     print(d)
     for x in range(5, 10):
-        if (x == 7): break
+        # if (x == 7): break
+        if (x % 2 == 0): continue
         print(x)
 
 
