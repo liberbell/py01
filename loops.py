5f8f2541d12d9e79c22326c1ce3f54a4769be351
Liberbell <liberbell@gmail.com>
2019-05-05 19:05:33 +0900

    loops3

--- loops.py
+++ loops.py
@@ -1,8 +1,8 @@
 def main():
     x = 0
-    while (x < 5):
-        print(x)
-        x = x + 1
+    # while (x < 5):
+    #     print(x)
+    #     x = x + 1
 
     for x in range(5, 10):
         print(x)
