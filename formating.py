0789c9ef1d39ba68e8b745bfd2dfae7fe8f9ce4e
Liberbell <liberbell@gmail.com>
2019-05-07 07:56:11 +0900

    formating7

--- formating.py
+++ formating.py
@@ -4,7 +4,7 @@ def main():
     now = datetime.now()
     print(now.strftime('The current year is %Y'))
     print(now.strftime('The current month is %b'))
-    print(now.strftime('%a, %d %B, %y'))
+    print(now.strftime('%a, %d %b, %y'))
 
 if __name__ == '__main__':
     main()
