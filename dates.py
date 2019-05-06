c98d1496ce6e0b9c44c538f79adba8ff9472b175
Liberbell <liberbell@gmail.com>
2019-05-07 07:43:36 +0900

    dates8

--- dates.py
+++ dates.py
@@ -16,6 +16,9 @@ def main():
     today = datetime.now()
     print('The current date and time is: ', today)
 
+    t = datetime.time(datetime.now())
+    print(t)
+
 
 if __name__ == '__main__':
     main()
