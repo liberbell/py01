9c3c49fb1afe8ac0c0e8fbdd6072abf8ef13a461
Liberbell <liberbell@gmail.com>
2019-05-06 16:47:13 +0900

    loops1

--- loops.py
+++ loops.py
@@ -10,10 +10,15 @@ def main():
     days = ["Mon", "Thu", "Wed", "Thu", "Fri", "Sat", "Sun"]
     # for d in days:
     #     print(d)
-    for x in range(5, 10):
+    # for x in range(5, 10):
         # if (x == 7): break
-        if (x % 2 == 0): continue
-        print(x)
+        # if (x % 2 == 0): continue
+        # print(x)
+
+    days = ["Mon", "Thu", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for i,d in enumerate(days):
+        print(d)
+
 
 
 if __name__ == '__main__':
