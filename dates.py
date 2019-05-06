5dd1ee45f2f235cb1892d4701c119948e558ad75
Liberbell <liberbell@gmail.com>
2019-05-07 07:41:23 +0900

    dates6

--- dates.py
+++ dates.py
@@ -3,15 +3,18 @@ from datetime import time
 from datetime import datetime
 
 def main():
-    today = date.today()
-    print('Today`s date is ', today)
+    # today = date.today()
+    # print('Today`s date is ', today)
+    #
+    # print('Date components:', today.day, today.month, today.year)
+    #
+    # print('Today`s weekday # is:', today.weekday())
+    #
+    # days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun']
+    # print('Which is a: ', days[today.weekday()])
 
-    print('Date components:', today.day, today.month, today.year)
-
-    print('Today`s weekday # is:', today.weekday())
-
-    days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun']
-    print('Which is a: ', days[today.weekday()])
+    today = datetime.now()
+    print(today)
 
 
 if __name__ == '__main__':
