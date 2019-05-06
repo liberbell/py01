d80c2413b74e479038562d22c6a4b92eff98a41a
Liberbell <liberbell@gmail.com>
2019-05-07 07:47:39 +0900

    formating4

--- formating.py
+++ formating.py
@@ -3,6 +3,7 @@ from datetime import datetime
 def main():
     now = datetime.now()
     print(now.strftime('The current year is %Y'))
+    print(now.strftime('The current month is %M'))
 
 if __name__ == '__main__':
     main()
