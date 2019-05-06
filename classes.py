477945ad5f440d936dc444dc8ca02153faea8c6d
Liberbell <liberbell@gmail.com>
2019-05-06 16:53:36 +0900

    classes3

--- classes.py
+++ classes.py
@@ -6,7 +6,9 @@ class myClass():
         print('myClass method2 ' + someString)
 
 def main():
-
+    c = myClass()
+    c.method1()
+    c.method2('This is a string')
 
 if __name__ == '__main__':
     main()
