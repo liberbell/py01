758b28d4f9b961b42eb687582e3807b236d2d327
Liberbell <liberbell@gmail.com>
2019-05-09 07:46:12 +0900

    timedeltas4

--- timedeltas.py
+++ timedeltas.py
@@ -19,3 +19,7 @@ print('This year april fool`s date: ' + str(afd))
 
 if afd < today:
     print('April Fool`s day already went by %d days ago' % (today - afd).days)
+    afd = afd.replace(year = today.year+1)
+
+time_to_afd = afd - today
+print('It`s just ', time_to_afd.days, 'days until April Fool`s day')
