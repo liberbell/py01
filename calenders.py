c85669c36c96bc209489074411109de1cedbfa95
Liberbell <liberbell@gmail.com>
2019-05-09 07:57:50 +0900

    calenders6

--- calenders.py
+++ calenders.py
@@ -1,9 +1,12 @@
 import calendar
 
-# c = calendar.TextCalendar(calendar.MONDAY)
+c = calendar.TextCalendar(calendar.MONDAY)
 # st = c.formatmonth(2019, 5, 0, 0)
 # print(st)
 
-hc = calendar.HTMLCalendar(calendar.SUNDAY)
-st = hc.formatmonth(2019, 5)
-print(st)
+# hc = calendar.HTMLCalendar(calendar.SUNDAY)
+# st = hc.formatmonth(2019, 5)
+# print(st)
+
+for i in c.itermonthdays(2019, 5):
+    print(i)
