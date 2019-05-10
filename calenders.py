c56caf7a260b48caf4181879b73d130c4f093027
Liberbell <liberbell@gmail.com>
2019-05-10 12:42:16 +0900

    calendars1

--- calenders.py
+++ calenders.py
@@ -11,8 +11,18 @@ c = calendar.TextCalendar(calendar.MONDAY)
 # for i in c.itermonthdays(2019, 5):
 #     print(i)
 
-for name in calendar.month_name:
-    print(name)
+# for name in calendar.month_name:
+#     print(name)
+#
+# for day in calendar.day_name:
 
-for day in calendar.day_name:
-    print(day)
+print('Team meeting will be on: ')
+for m in ragen(1, 13):
+    cal = calendar.monthcalendar(2019, m)
+    weekone = cal[0]
+    weektwo = cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
