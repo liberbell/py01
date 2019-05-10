d4d0c76cd2ebb587937dee3e2037fe9283486f8d
Liberbell <liberbell@gmail.com>
2019-05-10 12:48:20 +0900

    files4

--- files.py
+++ files.py
@@ -1,6 +1,11 @@
 def main():
     f = open('textfile.txt', 'w+')
 
+    for i in range(10):
+        f.write('This is line ' + str(i) + '\r\n')
+
+    f.close()
+
 
 if __name__ == '__main__':
     main()
