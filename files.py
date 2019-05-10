57af6de8be1a6da96e808e104dc0844672bfb808
Liberbell <liberbell@gmail.com>
2019-05-10 12:53:35 +0900

    files6

--- files.py
+++ files.py
@@ -2,10 +2,11 @@ def main():
     # f = open('textfile.txt', 'w+')
     f = open('textfile.txt', 'a')
 
-    for i in range(10):
-        f.write('This is line ' + str(i) + '\r\n')
-
-    f.close()
+    # for i in range(10):
+    #     f.write('This is line ' + str(i) + '\r\n')
+    #
+    # f.close()
+    
 
 
 if __name__ == '__main__':
