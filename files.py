235c655ff208d97e44f5f80ce372c0024e88e76a
Liberbell <liberbell@gmail.com>
2019-05-10 12:51:39 +0900

    files5

--- files.py
+++ files.py
@@ -1,5 +1,6 @@
 def main():
-    f = open('textfile.txt', 'w+')
+    # f = open('textfile.txt', 'w+')
+    f = open('textfile.txt', 'a')
 
     for i in range(10):
         f.write('This is line ' + str(i) + '\r\n')
