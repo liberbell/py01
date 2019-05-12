a18ff7c6ac3802409d48e64753bfa6e5f0af8028
Liberbell <liberbell@gmail.com>
2019-05-12 14:22:12 +0900

    inetdata4

--- inetdata.py
+++ inetdata.py
@@ -3,7 +3,10 @@ import urllib.request
 def main():
     weburl = 'https://www.google.com'
     result = urllib.request.urlopen(weburl)
+
     print('Result code: ' + str(result.getcode()))
+    data = result.read()
+    print(data)
 
 if __name__ == '__main__':
     main()
