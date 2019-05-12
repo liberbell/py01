e527b84c25734ff1ac3046f99bc5de3d5f5426f9
Liberbell <liberbell@gmail.com>
2019-05-12 14:35:59 +0900

    jsondata2

---
+++ jsondata.py
@@ -0,0 +1,13 @@
+from urllib import request
+
+def printResults(data):
+    theJson = json.loads(all)
+
+def main():
+    urlData = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson'
+
+    weburl = urllib.request.urlopen(urlData)
+    print('Result code: ' + str(weburl.getcode()))
+
+if __name__ == '__main__':
+    main()
