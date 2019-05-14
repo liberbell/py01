4c084f45f1f5c4d67e03df3d37bacddc38a3ac20
Liberbell <liberbell@gmail.com>
2019-05-14 11:19:31 +0900

    htmlparsing4

--- htmlparsing.py
+++ htmlparsing.py
@@ -4,6 +4,9 @@ class MyHTMLParser(HTMLParser)
 
 def main():
     parser = MyHTMLParser()
+    f = open('samplehtml.html')
+    if f.mode == 'r':
+        contents = f.read()
 
 
 if __name__ == '__main__':
