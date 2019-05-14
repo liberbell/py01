0f27c4db0ec2e812db7b83f070e1c84a62ff1fee
Liberbell <liberbell@gmail.com>
2019-05-15 07:57:22 +0900

    xmlparsing3

--- xmlparsing.py
+++ xmlparsing.py
@@ -2,6 +2,10 @@ import xml.dom.minidom
 
 
 def main():
+    doc = xml.dom.minidom.parse('samplexml.xml')
+
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
 
 if __name__ == '__main__':
     main()
